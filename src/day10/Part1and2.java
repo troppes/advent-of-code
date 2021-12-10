80997bea26cb62c3a312151af16c94e205a97ce0
Florian Reitz <flo@reitz.dev>
2021-12-10 12:28:09 +0100

    day10

--- src/day10/Part1and2.java
+++ src/day10/Part1and2.java
@@ -23,8 +23,8 @@ public class Part1and2 {
         }
         scoresIncomplete = scoresIncomplete.stream().sorted().filter(e -> e != 0).collect(Collectors.toCollection(ArrayList::new));
 
-        System.out.println("Part 1: "+scoreCorrupt);
-        System.out.println("Part 2: "+scoresIncomplete.get(scoresIncomplete.size()/2));
+        System.out.println("Part 1: " + scoreCorrupt);
+        System.out.println("Part 2: " + scoresIncomplete.get(scoresIncomplete.size() / 2));
     }
 
 
@@ -35,23 +35,31 @@ public class Part1and2 {
         Stack<Character> currentOpen = new Stack<>();
         for (Character c : list) {
             if (c == '[' || c == '{' || c == '<') {
-                currentOpen.push((char) (c+2));
-            } else if (c == '('){
-                currentOpen.push((char) (c+1));
+                currentOpen.push((char) (c + 2));
+            } else if (c == '(') {
+                currentOpen.push((char) (c + 1));
             } else {
                 if (!currentOpen.empty()) {
                     if (c != currentOpen.pop()) {
-                       switch (c){
-                           case ')': corruptedFound[0]++; break;
-                           case ']': corruptedFound[1]++; break;
-                           case '}': corruptedFound[2]++; break;
-                           case '>': corruptedFound[3]++; break;
-                       }
+                        switch (c) {
+                            case ')':
+                                corruptedFound[0]++;
+                                break;
+                            case ']':
+                                corruptedFound[1]++;
+                                break;
+                            case '}':
+                                corruptedFound[2]++;
+                                break;
+                            case '>':
+                                corruptedFound[3]++;
+                                break;
+                        }
                     }
                 }
             }
         }
-        return ((corruptedFound[0]*3)+(corruptedFound[1]*57)+(corruptedFound[2]*1197)+(corruptedFound[3]*25137));
+        return ((corruptedFound[0] * 3) + (corruptedFound[1] * 57) + (corruptedFound[2] * 1197) + (corruptedFound[3] * 25137));
     }
 
     static long findIncomplete(ArrayList<Character> list) {
@@ -59,9 +67,9 @@ public class Part1and2 {
         Stack<Character> currentOpen = new Stack<>();
         for (Character c : list) {
             if (c == '[' || c == '{' || c == '<') {
-                currentOpen.push((char) (c+2));
-            } else if (c == '('){
-                currentOpen.push((char) (c+1));
+                currentOpen.push((char) (c + 2));
+            } else if (c == '(') {
+                currentOpen.push((char) (c + 1));
             } else {
                 if (!currentOpen.empty()) {
                     if (c != currentOpen.pop()) {
@@ -72,13 +80,21 @@ public class Part1and2 {
             }
         }
         long score = 0;
-        while (!currentOpen.empty()){
+        while (!currentOpen.empty()) {
             score = score * 5;
-            switch (currentOpen.pop()){
-                case ')': score += 1; break;
-                case ']': score += 2; break;
-                case '}': score += 3; break;
-                case '>': score += 4; break;
+            switch (currentOpen.pop()) {
+                case ')':
+                    score += 1;
+                    break;
+                case ']':
+                    score += 2;
+                    break;
+                case '}':
+                    score += 3;
+                    break;
+                case '>':
+                    score += 4;
+                    break;
             }
         }
         return score;
