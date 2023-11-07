c1b94220b7fa947a45bc9c1bbebdc8799c68c51e
Florian Reitz <flo@reitz.dev>
2023-11-07 13:20:22 +0100

    added day2 2015

--- 2015/aoc/src/main/java/day02/day02.java
+++ 2015/aoc/src/main/java/day02/day02.java
@@ -1,13 +1,10 @@
 package day02;
 
-import com.sun.tools.jconsole.JConsoleContext;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
-import java.util.OptionalInt;
 
 public class day02 {
     public static void main(String[] args) throws IOException {
