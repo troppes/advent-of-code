cd215451ffd676f88ea0edd54157dd6593463ea1
Florian Reitz <flo@reitz.dev>
2023-12-14 13:46:07 +0100

    finished day14

--- 2023/day14/main.go
+++ 2023/day14/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"strings"
@@ -27,79 +28,154 @@ func main() {
 func part1(input string) int {
 	input = strings.ReplaceAll(input, "\r\n", "\n")
 	mirror := createMirror(input)
-	mirror = calcStonePositions(mirror)
+	mirror = moveStonesNorth(mirror)
 	sum := 0
 
-	for _, col := range mirror {
-		for i := len(col) - 1; i >= 0; i-- {
-			if col[i] == 'O' {
-				sum += i + 1
+	for x := range mirror {
+		for y := 0; y < len(mirror); y++ {
+			if mirror[y][x] == 'O' {
+				sum += len(mirror) - y
 			}
 		}
 	}
-
 	return sum
 }
 
 func part2(input string) int {
-	return part1(input)
-}
 
-func createMirror(input string) []string {
-	rows := strings.Split(input, "\n")
-	cols := make([]string, len(rows[0]))
+	input = strings.ReplaceAll(input, "\r\n", "\n")
+	mirror := createMirror(input)
 
-	for x := 0; x < len(rows[0]); x++ {
-		var sb strings.Builder
-		for y := 0; y < len(rows); y++ {
-			sb.WriteByte(rows[y][x])
+	var cache = make(map[string]int)
+	end := 1000000000
+
+	for i := 0; i < 1000000000; i++ {
+		mirror = moveStonesNorth(mirror)
+		mirror = moveStonesWest(mirror)
+		mirror = moveStonesSouth(mirror)
+		mirror = moveStonesEast(mirror)
+
+		key := string(bytes.Join(mirror, []byte{}))
+		if elem, ok := cache[key]; ok { // if found in cache
+			end = i + (1000000000-i)%(i-elem) - 1
+		} else { // if not add to cache
+			cache[key] = i
 		}
-		cols[x] = sb.String()
+
+		if i == end { // if we reached the retries needed, finish
+			break
+		}
 	}
 
-	return cols
+	sum := 0
+
+	for x := range mirror {
+		for y := 0; y < len(mirror); y++ {
+			if mirror[y][x] == 'O' {
+				sum += len(mirror) - y
+			}
+		}
+	}
+	return sum
 }
 
-func calcStonePositions(mirror []string) []string {
+func createMirror(input string) [][]byte {
 
-	newMirrors := make([]string, len(mirror))
+	inputArray := strings.Split(input, "\n")
+	grid := make([][]byte, len(inputArray))
 
-	// if the same stop otherwise
+	for y := 0; y < len(inputArray); y++ {
+		grid[y] = []byte(inputArray[y])
+	}
+
+	return grid
+}
+
+func moveStonesNorth(mirror [][]byte) [][]byte {
+	for x := 0; x < len(mirror[0]); x++ {
+		currCol := make([]byte, len(mirror))
+		for y := 0; y < len(currCol); y++ {
+			currCol[y] = mirror[y][x]
+		}
+
+		currCol = moveStones(currCol)
+
+		for y := 0; y < len(currCol); y++ {
+			mirror[y][x] = currCol[y]
+		}
+
+	}
+
+	return mirror
+}
+
+func moveStonesWest(mirror [][]byte) [][]byte {
 	for y := 0; y < len(mirror); y++ {
-		var sb strings.Builder
+		mirror[y] = moveStones(mirror[y])
+	}
+	return mirror
+}
 
-		currStones := 0
-		currCol := mirror[y]
-		for x := len(currCol) - 1; x >= 0; x-- { // change direction to change between south and north
-			if currCol[x] == 'O' {
-				currStones++
-				sb.WriteByte('.')
-			} else if currCol[x] == '#' {
-				// cut off sand for all stones
-				currString := sb.String()
-				sb.Reset()
-				sb.WriteString(currString[:len(currString)-currStones])
-				for i := 0; i < currStones; i++ {
-					sb.WriteByte('O')
-				}
-				currStones = 0
-				// place new cube rock
-				sb.WriteByte('#')
-			} else {
-				sb.WriteByte('.')
-			}
-		}
-		if currStones != 0 {
-			currString := sb.String()
-			sb.Reset()
-			sb.WriteString(currString[:len(currString)-currStones])
-			for i := 0; i < currStones; i++ {
-				sb.WriteByte('O')
-			}
-		}
+func moveStonesEast(mirror [][]byte) [][]byte {
+	for y := 0; y < len(mirror); y++ {
+		currRow := make([]byte, len(mirror[y]))
 
-		newMirrors[y] = sb.String()
+		for x := 0; x < len(currRow); x++ {
+			currRow[x] = mirror[y][len(currRow)-x-1]
+		}
+		currRow = moveStones(currRow)
+
+		for x := 0; x < len(currRow); x++ {
+			mirror[y][len(currRow)-x-1] = currRow[x]
+		}
 	}
 
-	return newMirrors
+	return mirror
+}
+
+func moveStonesSouth(mirror [][]byte) [][]byte {
+	for x := 0; x < len(mirror[0]); x++ {
+		currCol := make([]byte, len(mirror))
+		for y := 0; y < len(currCol); y++ {
+			currCol[y] = mirror[len(currCol)-y-1][x]
+		}
+
+		currCol = moveStones(currCol)
+
+		for y := 0; y < len(currCol); y++ {
+			mirror[len(currCol)-y-1][x] = currCol[y]
+		}
+
+	}
+
+	return mirror
+}
+
+// instead of moving the stones differently we change the input north => south in reverse etc
+func moveStones(row []byte) []byte {
+
+	currStones := 0
+
+	for x := len(row) - 1; x >= 0; x-- {
+		if row[x] == 'O' {
+			currStones++
+			row[x] = '.'
+		} else if row[x] == '#' {
+			// cut off sand for all stones
+			for i := 1; i <= currStones; i++ {
+				row[x+i] = 'O'
+			}
+			currStones = 0
+			// place new cube rock
+			row[x] = '#'
+		} else {
+			row[x] = '.'
+		}
+	}
+	if currStones != 0 {
+		for i := 0; i < currStones; i++ {
+			row[i] = 'O'
+		}
+	}
+	return row
 }
