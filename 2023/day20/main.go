6b0f2abd9b088fc750603c73860e8586f675d70e
Florian Reitz <flo@reitz.dev>
2023-12-21 16:55:35 +0100

    added day20 boilerplate

---
+++ 2023/day20/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/troppes/advent-of-code/util"
+)
+
+func main() {
+	var part int
+	flag.IntVar(&part, "part", 1, "part 1 or 2")
+	flag.Parse()
+
+	input := util.ReadInput("input.txt")
+
+	if part == 1 {
+		ans := part1(input)
+		fmt.Println("Output:", ans)
+	} else {
+		ans := part2(input)
+		fmt.Println("Output:", ans)
+	}
+}
+
+func part1(input string) int {
+	return 0
+}
+
+func part2(input string) int {
+	return 0
+}
